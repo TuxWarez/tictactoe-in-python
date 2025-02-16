f7610cb61fa84be8d8a77b4cda350451f69af2dc
TuxWare <71888789+TuxWarez@users.noreply.github.com>
2025-02-16 14:27:32 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,78 @@
 import random
-arr = [["." for j in range(3)] for i in range(3)]
-flag = False
+import os
+
+grid = [["." for j in range(3)] for i in range(3)]
+game_over = False
 x = 0
 char = 0
 allowed = ['1', '2', '3']
-list1 = [0, 1, 2]
+game_over_1 = False
 
-def playermove():
+def player_move():
     global xp
     global yp
-    y = input("insert a character in xcoord,ycoord format: ")
-    if len(y) > 3:
+    xy = input("insert a character in xcoord,ycoord format: ")
+    if len(xy) > 3:
         print("The length of input is too long; try again")
-        playermove()
-    if "," not in y:
+        player_move()
+    if "," not in xy:
         print("The format in input is wrong; try again")
-        playermove()
-    if y[0] not in allowed or y[2] not in allowed:
+        player_move()
+    if xy[0] not in allowed or xy[2] not in allowed:
         print("The numbers are out of range; try again")
-        playermove()
-    y = [int(i) - 1 for i in y.split(',')]
-    if arr[y[0]][y[1]] == 'X' or arr[y[0]][y[1]] == 'O':
+        player_move()
+    xy = [int(i) - 1 for i in xy.split(',')]
+    if grid[xy[0]][xy[1]] == 'X' or grid[xy[0]][xy[1]] == 'O':
         print("There's already a character there; try again")
-        playermove()
-    xp, yp = y[0], y[1]
-    return xp, yp
+        player_move()
+    grid[xy[0]][xy[1]] = char
 
-def cpumovebest(markmove):
+def cpu_move_best(markmove):
     global row
     global col
     for row in range(3):
         for col in range(3):
-            if arr[row][col] == '.':
-                arr[row][col] = markmove
+            if grid[row][col] == '.':
+                grid[row][col] = markmove
                 if check(markmove):
-                    arr[row][col] = '.'
-                    return row, col
-                arr[row][col] = '.'
+                    grid[row][col] = '.'
+                grid[row][col] = '.'
     return None
 
-def cpumoverandom():
+def cpu_move_random():
     global xcord
     global ycord
-    xcord = random.choice(list1)
-    ycord = random.choice(list1)
-    if arr[xcord][ycord] == 'X' or arr[xcord][ycord] == 'O':
-        cpumoverandom()
-    return xcord, ycord
+    xcord = random.randint(0, 2)
+    ycord = random.randint(0, 2)
+    if grid[xcord][ycord] == 'X' or grid[xcord][ycord] == 'O':
+        cpu_move_random()
+    grid[xcord][ycord] = cpuchar
 
-def cpumove():
-    move = cpumovebest(cpuchar)
+def cpu_move():
+    move = cpu_move_best(cpuchar)
     if move is not None:
-        arr[row][col] = cpuchar
-        return arr
-    move = cpumovebest(char)
-    print(move, row, col)
+        grid[row][col] = cpuchar
+        return grid
+    move = cpu_move_best(char)
     if move is not None:
-        arr[row][col] = cpuchar
-        return arr
-    cpumoverandom()
-    arr[xcord][ycord] = cpuchar
+        grid[row][col] = cpuchar
+        return grid
+    cpu_move_random()
 
-def prtable():
-    for i in arr:
+def print_table():
+    for i in grid:
         for j in i:
             print(f"{j}", end=' ')
         print()
 
 def check(mark):
-    if (arr[0][0] == mark and arr[1][1] == mark and arr[2][2] == mark) or (arr[0][2] == mark and arr[1][1] == mark and arr[2][0] == mark):
+    if (grid[0][0] == mark and grid[1][1] == mark and grid[2][2] == mark) or (grid[0][2] == mark and grid[1][1] == mark and grid[2][0] == mark):
         return True
     for i in range(3):
-        if arr[i][0] == mark and arr[i][1] == mark and arr[i][2] == mark:
+        if grid[i][0] == mark and grid[i][1] == mark and grid[i][2] == mark:
             return True
     for i in range(3):
-        if arr[0][i] == mark and arr[1][i] == mark and arr[2][i] == mark:
+        if grid[0][i] == mark and grid[1][i] == mark and grid[2][i] == mark:
             return True
     return False
 
@@ -84,70 +82,72 @@ def choose():
     char = input()
     if char == 'x' or char == 'o':
         return char
-    else:
-        print('Try again;')
-        choose()
-choose()
+    print('Try again;')
+    choose()
 
-def main():
+def play_turn():
     global char
     global x
     global cpuchar
-    global flag
+    global game_over
     if char == 'x':
-        cpuchar = 'O'
         char = 'X'
-        playermove()
-        arr[xp][yp] = char
-        flag = check(char)
+        player_move()
+        game_over = check(char)
         x = x + 1
         if x == 5:
-            return
-        if flag == True:
-            prtable()
-            return
+            return True
+        if game_over == True:
+            os.system('clear')
+            print_table()
+            return True
+        cpuchar = 'O'
         char = 'O'
-        cpumove()
-        prtable()
-        flag = check(cpuchar)
-        if flag == True:
-            return
+        cpu_move()
+        os.system('clear')
+        print_table()
+        game_over = check(cpuchar)
+        if game_over == True:
+            return True
         char = 'x'
-        main()
     elif char == 'o':
         cpuchar = 'X'
         char = 'X'
-        cpumove()
-        prtable()
-        flag = check(cpuchar)
-        if flag == True:
-            return
+        cpu_move()
+        os.system('clear')
+        print_table()
+        game_over = check(cpuchar)
+        if game_over == True:
+            return True
         char = 'O'
-        playermove()
-        arr[xp][yp] = char
-        flag = check(char)
+        player_move()
+        game_over = check(char)
         x = x + 1
         if x == 5:
-            return
-        if flag == True:
-            prtable()
-            return
+            return True
+        if game_over == True:
+            os.system('clear')
+            print_table()
+            return True
         char = 'o'
-        main()
-main()
 
-if char == 'X' and flag == True:
-    print("X won!")
-elif char == 'O' and flag == True:
-    print("O won!")
-if not flag:
+def print_result(): # for anybody reading this code tell my why this function stalls the program
+    if game_over:
+        print(f'{char} won!')
+        return
     print("Draw :(")
 
-replay = input("Do you want to play another game? [y/N] ")
-
-if replay == 'y':
-    flag = False
-    x = 0
-    arr = [["." for j in range(3)] for i in range(3)]
+def play_game():
     choose()
-    main()
+    grid = [["." for j in range(3)] for i in range(3)]
+    while True:
+        play_turn()
+        game_over_1 = play_turn()
+        if game_over_1:
+            return
+
+while True:
+    play_game()
+ #  print_result()
+    if input("Do you want to play another game? [y/N] ")!="y":
+        break
